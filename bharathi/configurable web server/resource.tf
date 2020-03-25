652c516468b1b84cbd4d467c91177143c9fcbbd4
bharathi tulabandu <61180873+bharathitulabandu@users.noreply.github.com>
2020-03-25 15:29:35 +0530

    Create resource.tf

---
+++ bharathi/configurable web server/resource.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "web_server" {
+ami = "ami-03b5297d565ef30a6"
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.webserverSG.id]
+  tags = {
+    Name = "single-web server"
+  }
+  user_data = <<-EOF
+            #!/bin/bash
+            yum install httpd
+            service start httpd
+            echo " hello world " > index.html
+            EOF
+}
+
+resource "aws_security_group" "websevrerSG" {
+  name = "single-web server SG"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.webserver.public_ip
+}
