b173a8feb49d09e540480142661c9287de0cbc8a
vishnuhs <47343722+vishnuhs@users.noreply.github.com>
2020-03-25 18:00:10 +0530

    Add files via upload

---
+++ Singserver.tf
@@ -0,0 +1,19 @@
+DEPLOYING A SINGLE SERVER:
+
+provider "aws" {
+  region   = "eu-west-2"
+  shared_credentials_file = "c:/Users/admin/.aws/credentials"
+  profile = "TechSNIPS"
+} 
+resource "aws_instance" "example" 
+{
+  ami         = "ami-0c09927662c939f41"
+  instance_type = "t2.micro"
+  tags {
+  name = "TESTVM"
+  }
+}
+
+$.\terraform.exe init
+$.\terraform.exe apply
+$.\terraform.exe show
